3512343631fde4ade365c1f0f8d829cd9c182a11
Maryam <maryamjaved.xo@gmail.com>
2023-09-19 19:24:18 +0500

    fixed

--- controllers/authController.js
+++ controllers/authController.js
@@ -32,6 +32,7 @@ const handleErrors = (err) => {
     return errors;
 };
 
+
 // create json web token
 const maxAge = 3 * 24 * 60 * 60;
 const createToken = (id) => {
@@ -46,30 +47,26 @@ module.exports.register_get = async (req, res) => {
         const user = await User.find({});
         if (user) {
             res.status(200).send(user);
+        } else {
+            res.status(200).json({ message: "user not found" });
         }
-
-        else {
-            res.status(200).json({ message: "user not found" })
-        }
-    }
-
-    catch (err) {
+    } catch (err) {
         res.status(200).json({ message: "internal server error" });
-        console.log(err);
-
     }
-
 };
-
 
 module.exports.register_post = async (req, res) => {
     const { name, email, password, address, profilePic } = req.body;
 
     try {
         const user = await User.create({
-            name, email, password, address, profilePic
-
+            name,
+            email,
+            password,
+            address,
+            profilePic,
         });
+
         const token = createToken(user._id);
         res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
         res.status(201).json({ message: "Registered Successfully" });
@@ -84,7 +81,9 @@ module.exports.login_post = async (req, res) => {
     const { email, password } = req.body;
     try {
         if (!email || !password) {
-            return res.status(400).json({ message: "email and password are required" });
+            return res
+                .status(400)
+                .json({ message: "email and password are required" });
         }
         const user = await User.login(email, password);
         if (!user) {
@@ -100,19 +99,14 @@ module.exports.login_post = async (req, res) => {
 };
 
 
+
 module.exports.logout_get = (req, res) => {
     const user = User.findById(req.params.id);
     if (user) {
         res.status(200);
         res.cookie("jwt", "", { maxAge: 1 });
         res.json({ message: "user logged out" });
+    } else {
+        res.status(400).json({ message: "user not found" });
     }
-
-    else {
-        res.sttaus(400).json({ message: "user not found" })
-    }
-
 };
-
-
-
