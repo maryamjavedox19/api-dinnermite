08e6bc3353305acd28aaa0114599a0299b678f65
Maryam <maryamjaved.xo@gmail.com>
2023-10-17 01:14:39 +0500

    firebase

---
+++ config/firebaseConfig.js
@@ -0,0 +1,10 @@
+module.exports.firebaseConfig = {
+  apiKey: "AIzaSyABItxgkREx7wjZkTgJ3kDJzrlEA2CYeDY",
+  authDomain: "productimage-9cf8c.firebaseapp.com",
+  projectId: "productimage-9cf8c",
+  storageBucket: "productimage-9cf8c.appspot.com",
+  messagingSenderId: "670029911515",
+  appId: "1:670029911515:web:92c09b52604baac64283fe",
+  measurementId: "G-8HWT09VN97",
+
+};
--- controllers/productController.js
+++ controllers/productController.js
@@ -2,6 +2,17 @@ const Product = require("../models/productSchema");
 const asyncHandler = require("express-async-handler");
 const multer = require("multer");
 const ApiFeatures = require("../utils/apifeatures");
+const { initializeApp } = require("firebase/app");
+const { getStorage, ref, getDownloadURL, uploadBytesResumable } = require("firebase/storage");
+const { getAnalytics } = require("firebase/analytics");
+const config = require("../config/firebaseConfig.js");
+
+initializeApp(config.firebaseConfig);
+
+const storage = getStorage();
+module.exports.upload = multer({ storage: multer.memoryStorage() });
+
+
 
 // get all products
 module.exports.getProducts = asyncHandler(async (req, res) => {
@@ -30,29 +41,106 @@ module.exports.getProductsById = asyncHandler(async (req, res) => {
 
 
 // // saving images
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "uploads/");
-        // err, destination
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now();
-        cb(null, uniqueSuffix + file.originalname);
-    },
-});
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, "uploads/");
+//         // err, destination
+//     },
+//     filename: function (req, file, cb) {
+//         const uniqueSuffix = Date.now();
+//         cb(null, uniqueSuffix + file.originalname);
+//     },
+// });
 
-module.exports.upload = multer({ storage: storage });
+// module.exports.upload = multer({ storage: storage });
 
 
 // create product
+// module.exports.addProduct = async (req, res) => {
+//     try {
+//         const { title, category, subCategories, inStock, description, price } = req.body;
+//         const thumbnail = req.file;
+
+//         // if (!title || !category || !subCategories || !inStock || !description || !price || !thumbnail) {
+//         //     return res.status(400).send("Missing required credentials");
+//         // }
+//     
+//         if (thumbnail) {
+//             const dateTime = giveCurrentDateTime();
+
+//             const storageRef = ref(storage, `files/${req.file.originalname + "       " + dateTime}`);
+
+//             // Create file metadata including the content type
+//             const metadata = {
+//                 contentType: req.file.mimetype,
+//             };
+
+//             // Upload the file in the bucket storage
+//             const snapshot = await uploadBytesResumable(storageRef, req.file.buffer, metadata);
+//             //by using uploadBytesResumable we can control the progress of uploading like pause, resume, cancel
+
+//             // Grab the public url
+//             const downloadURL = await getDownloadURL(snapshot.ref);
+
+//             console.log('File successfully uploaded.');
+//             return res.send({
+//                 message: 'file uploaded to firebase storage',
+//                 name: req.file.originalname,
+//                 type: req.file.mimetype,
+//                 downloadURL: downloadURL
+//             })
+
+//         }
+
+
+//         const productData = {
+//             title,
+//             category,
+//             subCategories,
+//             inStock,
+//             description,
+//             price,
+//             thumbnail: downloadURL,
+
+//         }
+
+//         const product = await Product.create(productData);
+
+//         res.status(201).send("Product created successfully");
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).send("Internal server error");
+//     }
+// };
+
+
 module.exports.addProduct = async (req, res) => {
     try {
         const { title, category, subCategories, inStock, description, price } = req.body;
         const thumbnail = req.file;
 
-        // if (!title || !category || !subCategories || !inStock || !description || !price || !thumbnail) {
-        //     return res.status(400).send("Missing required credentials");
-        // }
+        if (!title || !category || !subCategories || !inStock || !description || !price || !thumbnail) {
+            return res.status(400).send("Missing required credentials");
+        }
+
+        let downloadURL = '';
+
+        if (thumbnail) {
+            const dateTime = giveCurrentDateTime();
+
+            const storageRef = ref(storage, `files/${req.file.originalname + "       " + dateTime}`);
+
+            const metadata = {
+                contentType: req.file.mimetype,
+            };
+
+            const snapshot = await uploadBytesResumable(storageRef, req.file.buffer, metadata);
+
+            downloadURL = await getDownloadURL(snapshot.ref);
+
+            console.log('File successfully uploaded.');
+        }
+
         const productData = {
             title,
             category,
@@ -60,13 +148,8 @@ module.exports.addProduct = async (req, res) => {
             inStock,
             description,
             price,
-            // thumbnail: thumbnail.filename
-
-        }
-
-        if (thumbnail) {
-            productData.thumbnail = thumbnail.filename
-        }
+            thumbnail: downloadURL,
+        };
 
         const product = await Product.create(productData);
 
@@ -76,6 +159,16 @@ module.exports.addProduct = async (req, res) => {
         res.status(500).send("Internal server error");
     }
 };
+
+
+const giveCurrentDateTime = () => {
+    const today = new Date();
+    const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    const dateTime = date + ' ' + time;
+    return dateTime;
+}
+
 
 
 // update product
