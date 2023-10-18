b1263091d3d2e32ceab440957d14b2f5d9977b6f
Maryam <maryamjaved.xo@gmail.com>
2023-10-18 10:50:33 +0500

    body parser

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ const dotenv = require("dotenv").config();
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.set('view engine', 'ejs');
 
 
